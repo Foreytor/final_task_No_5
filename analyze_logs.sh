f75771b84f6aa320f7e5f783d27c159cdd8bd51c
Foreytor <Firegek@gmail.com>
2024-08-19 00:07:38 +0300

    feat: add 3 and 4 feat

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -25,5 +25,32 @@ unique_ips=$(awk '{print $1}' access.log | sort | uniq | wc -l)
 
 echo Количество уникальных IP-адресов     $unique_ips >> $out_name
 
+echo Количество запросов по методам: >> $out_name
+
+# Подсчет запросов по методам с использованием awk
+awk '{ 
+  # Извлекаем метод, удаляя начальную и конечную кавычки
+  method = substr($6, 2, length($6) - 1) 
+  count[method]++ 
+} 
+END { 
+  for (method in count) { 
+    print "   " count[method], method
+  } 
+}' access.log >> $out_name
+
+
+# Находим самый популярный URL с использованием awk
+popular_url=$(awk '{urls[$7]++;} END {
+                    for (url in urls) {
+                            if (max_count < urls[url]) {
+                                max_count = urls[url]; popular_url = url;
+                            }
+                        } print "  " max_count " - " popular_url
+                    }' access.log) 
+
+# Выводим самый популярный URL
+echo "Самый популярный URL: $popular_url" >> $out_name
+
 # Сообщение об выполнении
 echo "Ответ сохранен в файле $out_name"
